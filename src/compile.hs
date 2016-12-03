c1accaf4bcd7b76890089b5633174b28d80fe1d9
JosephLandy <josephlandy@Josephs-MacBook-Pro.local>
2016-12-03 18:37:30 -0500

    fuck if I know!

--- src/compile.hs
+++ src/compile.hs
@@ -1,6 +1,14 @@
-module compile where
-
--- takes a complete C  file and returns a complete VHDL file
-compMain :: String -> String
+module Compile (compMain) where
 
 
+
+  --maintain a symbol table!
+  --a variable is a pair of type and id.
+  type Var = (String, Integer)
+  -- elements:
+  --declarations
+  --assignments
+  --control blocks
+
+  -- takes a complete C  file and returns a complete VHDL file as strings.
+  -- compMain :: String -> String
